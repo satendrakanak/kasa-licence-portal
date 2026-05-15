57600cb60dc47ab06c98aa20e1fef05a0c56596e
Satendra <gangwarsatendra.kcmt@gmail.com>
2026-05-15 16:07:48 +0530

    Rename portal deployment to KASA Admin

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "kasa-licence-portal",
+      name: "kasa-admin-portal",
       script: "npm",
       args: "run start",
       cwd: "/opt/kasa/kasa-licence-portal",
